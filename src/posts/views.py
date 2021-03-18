539a1ff4a3a59e4d596c39dd3a42c8410835fd82
Bekzhan <bidahmetov2002@gmail.com>
2021-03-18 21:55:44 +0100

    Posts forms.py was created. Post form, comment form. Displaing forms. Handling post requests

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,16 +1,42 @@
 from django.shortcuts import render, redirect
 from .models import Post, Like
 from profiles.models import Profile
+from .forms import PostModelForm, CommentModelForm
 # Create your views here.
 
 def post_comment_create_and_list_view(request):
 	qs = Post.objects.all()
 	profile = Profile.objects.get(user=request.user)
+ 
+	p_form = PostModelForm()
+	c_form = CommentModelForm()
+	post_added = False
+
+	if 'submit_p_form' in request.POST:
+		p_form = PostModelForm(request.POST, request.FILES)
+		if p_form.is_valid():
+			ins = p_form.save(commit=False)
+			ins.author = profile
+			ins.save()
+			p_form = PostModelForm()
+			post_added = True
+
+	if 'submit_c_form' in request.POST:
+		c_form = CommentModelForm(request.POST)
+		if c_form.is_valid():
+			ins = c_form.save(commit=False)
+			ins.user = profile
+			ins.post = Post.objects.get(id=request.POST.get('post_id'))
+			ins.save()
+			c_form = CommentModelForm()
 
 
 	ctx = {
 		'qs': qs,
 		'profile': profile,
+		'p_form': p_form,
+		'c_form': c_form,
+		'post_added': post_added,
 	}
 
 	return render(request, 'posts/main.html', ctx)

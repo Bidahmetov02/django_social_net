0cb47b4b2231f2b3a4d2dd782b90a9773035c6cd
Bekzhan <bidahmetov2002@gmail.com>
2021-03-20 22:38:05 +0100

    Generic class-based List view for profiles. Add friend, Remove, Waiting buttons

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Profile, Relationship
 from .forms import ProfileModelForm
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
-# Create your views here.
 def my_profile_view(request):
 	profile = Profile.objects.get(user=request.user)
 	form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
@@ -50,3 +51,36 @@ def invite_profiles_list_view(request):
 	}
 
 	return render(request, 'profiles/to_invite_list.html', ctx)
+
+class ProfileListView(ListView):
+	model = Profile
+	template_name = 'profiles/profile_list.html'
+	context_object_name = 'qs'
+
+	def get_queryset(self):
+		qs = Profile.objects.get_all_profiles(self.request.user)
+
+		return qs
+
+	def get_context_data(self, **kwargs):
+	    context = super().get_context_data(**kwargs)
+	    user = User.objects.get(username__iexact=self.request.user)
+	    profile = Profile.objects.get(user=user)
+	    rel_r = Relationship.objects.filter(sender=profile)
+	    rel_s = Relationship.objects.filter(receiver=profile)
+	    rel_receiver = []
+	    rel_sender = []
+
+	    for item in rel_r:
+	    	rel_receiver.append(item.receiver.user) 
+
+	    for item in rel_s:
+	    	rel_sender.append(item.sender.user)
+
+	    context['rel_receiver'] = rel_receiver
+	    context['rel_sender'] = rel_sender
+	    context['is_empty'] = False
+	    if len(self.get_queryset()) == 0:
+	    	context['is_empty'] = True
+
+	    return context

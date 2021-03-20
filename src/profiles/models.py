2acc729b784d073a5c6745953bb2ef94c403b065
Bekzhan <bidahmetov2002@gmail.com>
2021-03-20 12:18:05 +0100

    profiles.models managers. New templates

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -2,7 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
+from django.db.models import Q
 # Create your models here.
+
+class ProfileManager(models.Manager):
+
+	def get_all_profiles_to_invite(self, sender):
+		profiles = Profile.objects.all().exclude(user=sender)
+		profile = Profile.objects.get(user=sender)
+		qs = Relationship.objects.filter(Q(sender=profile) | Q(receiver=profile))
+		print(qs)
+		print("###############")
+
+		accepted = set([])
+
+		for rel in qs:
+			if rel.status == 'accepted':
+				accepted.add(rel.receiver)
+				accepted.add(rel.sender)
+		print(accepted)
+		print("###############")
+
+		available = [] 
+
+		for p in profiles:
+			if p not in accepted:
+				available.append(p)
+
+		print(available)
+		print("###############")
+
+		return available
+
+	def get_all_profiles(self, me):
+		profiles = Profile.objects.all().exclude(user=me)
+		return profiles
+
 class Profile(models.Model):
 	first_name = models.CharField(max_length=200, blank=True)
 	last_name  = models.CharField(max_length=200, blank=True)
@@ -15,6 +50,8 @@ class Profile(models.Model):
 	slug	   = models.SlugField(unique=True, blank=True)
 	updated	   = models.DateTimeField(auto_now=True)
 	created    = models.DateTimeField(auto_now_add=True)
+
+	objects = ProfileManager()
 
 	def get_friends(self):
 		return self.friends.all()
@@ -64,12 +101,19 @@ STATUS_CHOICES = (
 	('accepted', 'accepted')
 )
 
+class RelationshipManager(models.Manager):
+	def invatations_received(self, receiver):
+		qs = Relationship.objects.filter(receiver=receiver, status='sent')
+		return qs
+
 class Relationship(models.Model):
     sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    objects = RelationshipManager()
 
     def __str__(self):
         return f"{self.sender}-{self.receiver}-{self.status}"
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Relationship
 from .forms import ProfileModelForm
 
 # Create your views here.
@@ -20,3 +20,33 @@ def my_profile_view(request):
 	}
 
 	return render(request, 'profiles/myprofile.html', ctx)
+
+def invites_received_view(request):
+	profile = Profile.objects.get(user=request.user)
+	qs = Relationship.objects.invatations_received(profile)
+
+	ctx = {
+		'qs': qs
+	}
+
+	return render(request, 'profiles/my_invites.html', ctx)
+
+def profiles_list_view(request):
+	user = request.user
+	qs = Profile.objects.get_all_profiles(user)
+
+	ctx = {
+		'qs': qs
+	}
+
+	return render(request, 'profiles/profile_list.html', ctx)
+
+def invite_profiles_list_view(request):
+	user = request.user
+	qs = Profile.objects.get_all_profiles_to_invite(user)
+
+	ctx = {
+		'qs': qs
+	}
+
+	return render(request, 'profiles/to_invite_list.html', ctx)

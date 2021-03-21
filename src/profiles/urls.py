9130b71a177776af84088fd825f2cb8aaf2c41b8
Bekzhan <bidahmetov2002@gmail.com>
2021-03-21 21:11:49 +0100

    Fixed bug with slug creation. Detail view, template. New method on profiles. See profile button href

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -8,18 +8,19 @@ from .views import (
 	remove_from_friends,
 	accept_invatation,
 	reject_invatation,
+	ProfileDetailView,
 )
 
 app_name = 'profiles'
 
 urlpatterns = [
+	path('', ProfileListView.as_view(), name="all-profiles-view"),
 	path('myprofile/', my_profile_view, name="my-profile-view"),
 	path('my-invites/', invites_received_view, name="invites-received-view"),
-	path('all-profiles/', ProfileListView.as_view(), name="all-profiles-view"),
 	path('to-invite/', invite_profiles_list_view, name="invite-profiles-view"),
 	path('send-invite/', send_invatation, name="send-invite"),
 	path('remove-friend/', remove_from_friends, name="remove-friend"),
 	path('my-invites/accept', accept_invatation, name="accept-invite"),
 	path('my-invites/reject', reject_invatation, name="reject-invite"),
-
+	path('<slug>', ProfileDetailView.as_view(), name='profile-detail-view'),
 ]
